18202df1f5e502dafab3451757e8f5e1033dbb75
ngocbao <ngocbao24ngoc@gmail.com>
2024-04-23 13:14:31 +0700

    commit

---
+++ src/Server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const configViewEngine = require('./config/viewEngine');
+const webRouters = require('./routes/web');
+const connection = require('./config/database');
+
+const app = express()
+const PORT = process.env.PORT || 8888;
+const HOST_NAME = process.env.HOST_NAME;
+
+//Routers
+configViewEngine(app);
+app.use('/',webRouters);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
