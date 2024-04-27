42a753102cfd17f14951deb0ae3de2493a8134c6
DaoAnhThien <22521382@gm.uit.edu.com>
2024-04-27 09:53:22 +0700

    editHomepage

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
-const mysql = require('mysql2');
+//require('dotenv').config()
+//const mysql = require('mysql2');
 
-const connection = mysql.createConnection(
-    {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    }
-);
+///const connection = mysql.createConnection(
+    //{
+       //host: process.env.DB_HOST,
+        //user: process.env.DB_USER,
+        //password: process.env.DB_PASS,
+        //database: process.env.DB_NAME
+    //}
+//);
 
-module.exports = connection;
+//module.exports = connection;
