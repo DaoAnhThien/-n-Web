77bd4332045c132342b2d7918fedfcc4e81c3695
gunnersarus <thanhptt1337@gmail.com>
2024-05-07 09:47:22 +0700

    update bang chi tiet

--- src/public/js/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".scroll-container").slick({
-        slidesToShow: 3,
-        infinite: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 700,
-    });
-  });
