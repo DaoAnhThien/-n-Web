7cae9eede76266ecf239691ecf0baa0b7a4bf6d0
Van Truong <114815683+paultheal1en@users.noreply.github.com>
2024-05-11 03:16:16 +0700

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,9 +4,13 @@ const {HandleRegister} = require('../controllers/apiController');
 const {HandleLogin} = require('../controllers/apiController');
 const {HandleForgotPassword} = require('../controllers/apiController');
 const {HandleForgotPasswordConfirm} = require('../controllers/apiController');
+const {HandleFavouriteTrick} = require('../controllers/apiController');
+const {HandleFavouriteFood} = require('../controllers/apiController');
 
 router.post('/Login',HandleLogin);
 router.post('/Register',HandleRegister);
 router.post('/ForgotPassword',HandleForgotPassword);
 router.post('/ForgotPassword/Confirm',HandleForgotPasswordConfirm);
+router.post('/FavouritesTrick',HandleFavouriteTrick);
+router.post('/FavouritesFood',HandleFavouriteFood);
 module.exports = router; 
