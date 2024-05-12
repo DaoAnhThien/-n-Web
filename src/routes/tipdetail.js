169903d4896817512bd1c15a64e37ad964493a77
gunnersarus <thanhptt1337@gmail.com>
2024-05-12 13:30:12 +0700

    update trai tim

--- src/routes/tipdetail.js
+++
@@ -1,9 +0,0 @@
-const express =require('express');
-const router=express.Router();
-
-const tipcontroller = require('../controllers/tipdetailController');
-
-router.get('/:slug',tipcontroller.show);
-
-
-module.exports= router;
