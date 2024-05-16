4c7aad02ff332508cb6e087aad62c084671ff189
ngocbao <ngocbao24ngoc@gmail.com>
2024-05-17 05:24:56 +0700

    Final update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,57 +1,57 @@
 const axios = require('axios');
 const connection = require('../config/database');
-const {isLogging} = require('../models/User');
-const getHomepage = (req,res) =>{
+const { isLogging } = require('../models/User');
+const getHomepage = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('Homepage.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('Homepage.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('Homepage.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getLogin = (req,res) => {
+const getLogin = (req, res) => {
   res.render('Login.ejs');
 }
-const getRegister = (req,res) => {
+const getRegister = (req, res) => {
   res.render('SignUp.ejs');
 }
-const getForgotPassword = (req,res) => {
+const getForgotPassword = (req, res) => {
   res.render('Forgotpassword.ejs')
 }
-const getMeoVat = (req,res,next) => {
+const getMeoVat = (req, res, next) => {
   const loggedIn = req.session.user ? true : false;
   let page = parseInt(req.params.page);
-  let limit=9;
-  let offset= (!page||page<1)?0:(page-1)*limit;
+  let limit = 9;
+  let offset = (!page || page < 1) ? 0 : (page - 1) * limit;
   let user = [];
   connection.connect((err) => {
-      if (err) {
-        console.error('Error connecting to MySQL database: ' + err.stack);
-        return;
-      }
-      console.log('Connected to MySQL database!');
-      //Thực hiện truy vấn sau khi kết nối đã thành công
-      connection.query(
-        `SELECT * From meo LIMIT ${limit} OFFSET ${offset}`,
-        function (err, result, fields) {
-          user = result;
-          if (err) {
-            console.error('Error executing query: ' + err.stack);
-            return;
-          }
-          console.log(">>>result= ", user);
-          //res.render('meovat.handlebars',{user})
-          if (loggedIn) {
-            const username = req.session.user.username
-            res.render('meovat.handlebars', {user, loggedIn: true, username: username });
+    if (err) {
+      console.error('Error connecting to MySQL database: ' + err.stack);
+      return;
+    }
+    console.log('Connected to MySQL database!');
+    //Thực hiện truy vấn sau khi kết nối đã thành công
+    connection.query(
+      `SELECT * From meo LIMIT ${limit} OFFSET ${offset}`,
+      function (err, result, fields) {
+        user = result;
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          return;
+        }
+        console.log(">>>result= ", user);
+        //res.render('meovat.handlebars',{user})
+        if (loggedIn) {
+          const username = req.session.user.username
+          res.render('meovat.handlebars', { user, loggedIn: true, username: username });
         } else {
-            res.render('meovat.handlebars',{user,loggedIn: false,username: null });
+          res.render('meovat.handlebars', { user, loggedIn: false, username: null });
         }
-        }
-      );
-    })
-   
+      }
+    );
+  })
+
 }
 const getMeo = (req, res, next) => {
   const loggedIn = req.session.user ? true : false;
@@ -88,290 +88,263 @@ const getMeo = (req, res, next) => {
     );
   })
 }
-const getProfile = (req,res) => {
+const getProfile = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('Userpage.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('Userpage.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('Userpage.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const get4meobienthitdaithanhthitmem = (req,res) => {
+const get4meobienthitdaithanhthitmem = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('4meobienthitdaithanhthitmem.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('4meobienthitdaithanhthitmem.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('4meobienthitdaithanhthitmem.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const get6luuychonguoimoibatdau = (req,res) => {
+const get6luuychonguoimoibatdau = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('6luuychonguoimoibatdau.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('6luuychonguoimoibatdau.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('6luuychonguoimoibatdau.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const get6Skillslambep = (req,res) => {
+const get6Skillslambep = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('6Skillslambep.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('6Skillslambep.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('6Skillslambep.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const get10bikipchonthucphamtuoi = (req,res) => {
+const get10bikipchonthucphamtuoi = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('10bikipchonthucphamtuoi.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('10bikipchonthucphamtuoi.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('10bikipchonthucphamtuoi.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getCachlamsangamdunnuocdien = (req,res) => {
+const getCachlamsangamdunnuocdien = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('Cachlamsangamdunnuocdien.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('Cachlamsangamdunnuocdien.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('Cachlamsangamdunnuocdien.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getNauanvoingucoc = (req,res) => {
+const getNauanvoingucoc = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('Nauanvoingucoc.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('Nauanvoingucoc.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('Nauanvoingucoc.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getBuaSang = (req,res) => {
+const getBuaSang = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   let page = parseInt(req.params.page);
   let user = [];
   let img = [];
-  connection.connect((err)=>{
-    if(err){
-      console.error('Error connecting to MySQL database' +err.stack)
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database' + err.stack)
       return;
     }
     console.log('Connect to database succesfully!');
-    const limit=6;
+    const limit = 6;
     //const offset=1;
-    const offset=(!page||page<=1)?0:(page-1)*6;
+    const offset = (!page || page <= 1) ? 0 : (page - 1) * 6;
     connection.query(
-    `SELECT * from bua_sang LIMIT ${limit} OFFSET ${offset}`,
-    function (err,result,fields){
-      user=result;
-      if (err) {
-        console.error('Error executing query: ' + err.stack);
-        return;
+      `SELECT * from bua_sang LIMIT ${limit} OFFSET ${offset}`,
+      function (err, result, fields) {
+        user = result;
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          return;
+        }
+        console.log(">>>result= ", page);
+        if (loggedIn) {
+          const username = req.session.user.username
+          res.render('buasang.handlebars', { user, loggedIn: true, username: username });
+        } else {
+          res.render('buasang.handlebars', { user, loggedIn: false, username: null });
+        }
       }
-      console.log(">>>result= ", page);
-      if (loggedIn) {
-        const username = req.session.user.username
-        res.render('buasang.handlebars', {user, loggedIn: true, username: username });
-    } else {
-        res.render('buasang.handlebars',{user,loggedIn: false,username: null });
-    }
-    }
     );
   })
 }
-const getMonansang = (req,res,next) => {
+const getMonansang = (req, res, next) => {
   const loggedIn = req.session.user ? true : false;
-  const slug=req.params.SLUG;
+  const slug = req.params.SLUG;
   let user = [];
   let img = [];
-  connection.connect((err)=>{
-    if(err){
-      console.error('Error connecting to MySQL database' +err.stack)
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database' + err.stack)
       return;
     }
     console.log('Connect to database succesfully!');
     connection.query(
-    'SELECT * from bua_sang Where SLUG=?', [slug],
-    function (err,result,fields){
-      let id=result.ID;
-      'SELECT * from image_ctmeo where ID_CTMEO=?',[id],
-      function (err,result,fields){
-            img=result;
+      'SELECT * from bua_sang Where SLUG=?', [slug],
+      function (err, result, fields) {
+        let id = result.ID;
+        'SELECT * from image_ctmeo where ID_CTMEO=?', [id],
+          function (err, result, fields) {
+            img = result;
+          }
+        user = result;
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          return;
+        }
+        console.log(">>>result= ", user)
+        if (loggedIn) {
+          const username = req.session.user.username
+          res.render('chitietmonan.handlebars', { user, loggedIn: true, username: username });
+        } else {
+          res.render('chitietmonan.handlebars', { user, loggedIn: false, username: null });
+        }
       }
-      user=result;
-      if (err) {
-        console.error('Error executing query: ' + err.stack);
-        return;
-      }
-      console.log(">>>result= ", user)
-      if (loggedIn) {
-        const username = req.session.user.username
-        res.render('chitietmonan.handlebars', {user, loggedIn: true, username: username });
-    } else {
-        res.render('chitietmonan.handlebars',{user,loggedIn: false,username: null });
-    }
-    }
     );
   })
 }
-const getBuaTrua = (req,res) => {
+const getBuaTrua = (req, res) => {
   const loggedIn = req.session.user ? true : false;
-  let page= parseInt(req.params.page);
+  let page = parseInt(req.params.page);
   let user = [];
   let img = [];
-  connection.connect((err)=>{
-    if(err){
-      console.error('Error connecting to MySQL database' +err.stack)
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database' + err.stack)
       return;
     }
     console.log('Connect to database succesfully!');
-    let limit=6;
-    let offset=(!page||page<1)?0:(page-1)*6
+    let limit = 6;
+    let offset = (!page || page < 1) ? 0 : (page - 1) * 6
     connection.query(
-    `SELECT * from bua_trua LIMIT ${limit} OFFSET ${offset}`,
-    function (err,result,fields){
-      user=result;
-      if (err) {
-        console.error('Error executing query: ' + err.stack);
-        return;
+      `SELECT * from bua_trua LIMIT ${limit} OFFSET ${offset}`,
+      function (err, result, fields) {
+        user = result;
+        if (err) {
+          console.error('Error executing query: ' + err.stack);
+          return;
+        }
+        console.log(">>>result= ", img);
+        const loggedIn = req.session.user ? true : false;
+        if (loggedIn) {
+          const username = req.session.user.username
+          res.render('buatrua.handlebars', { user, loggedIn: true, username: username });
+        } else {
+          res.render('buatrua.handlebars', { user, loggedIn: false, username: null });
+        }
       }
-      console.log(">>>result= ", img);
-      const loggedIn = req.session.user ? true : false;
-      if (loggedIn) {
-        const username = req.session.user.username
-        res.render('buatrua.handlebars', { user,loggedIn: true, username: username });
-    } else {
-        res.render('buatrua.handlebars', {user, loggedIn: false,username: null });
-    }
-    }
     );
   })
 }
 
-// const getMonantrua = (req,res) =>{
-//   const loggedIn = req.session.user ? true : false;
-//   const slug=req.params.SLUG;
-//   let user = [];
-
-//   connection.connect((err)=>{
-//     if(err){
-//       console.error('Error connecting to MySQL database' +err.stack)
-//       return;
-//     }
-//     console.log('Connect to database succesfully!');
-//     connection.query(
-//     'SELECT * from bua_trua Where SLUG=?', [slug],
-//     function (err,result,fields){
-//       user = result;
-//       console.log(">>>result= ", user);
-//       if (loggedIn) {
-//         const username = req.session.user.username
-//         res.render('chitietmonan.handlebars', { user,loggedIn: true, username: username });
-//     } else {
-//         res.render('chitietmonan.handlebars', {user, loggedIn: false,username: null });
-//     }
-//     }
-//     );
-//   })
-
-// }
-const getMonantrua = (req,res) =>{
+const getMonantrua = (req, res) => {
   const loggedIn = req.session.user ? true : false;
-  const slug=req.params.SLUG;
+  const slug = req.params.SLUG;
   let user = [];
-  connection.connect((err)=>{
-    if(err){
-      console.error('Error connecting to MySQL database' +err.stack)
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL database' + err.stack)
       return;
     }
     console.log('Connect to database succesfully!');
     connection.query(
-    'SELECT * from bua_trua Where SLUG=?', [slug],
-    function (err,result,fields){
-      user = result;
-      console.log(">>>result= ", user);
-      if (loggedIn) {
-        const username = req.session.user.username
-        res.render('chitietmonan.handlebars', { user,loggedIn: true, username: username });
-    } else {
-        res.render('chitietmonan.handlebars', {user, loggedIn: false,username: null });
-    }
-    }
+      'SELECT * from bua_trua Where SLUG=?', [slug],
+      function (err, result, fields) {
+        user = result;
+        console.log(">>>result= ", user);
+        if (loggedIn) {
+          const username = req.session.user.username
+          res.render('chitietmonan.handlebars', { user, loggedIn: true, username: username });
+        } else {
+          res.render('chitietmonan.handlebars', { user, loggedIn: false, username: null });
+        }
+      }
     );
   })
-     
+
 }
 
-const getbanhbao = (req,res) => {
+const getbanhbao = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('banhbao.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('banhbao.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('banhbao.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getbanhtrungthu = (req,res) => {
+const getbanhtrungthu = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('banhtrungthu.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('banhtrungthu.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('banhtrungthu.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getbunca = (req,res) => {
+const getbunca = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('bunca.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('bunca.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('bunca.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getburntcheesecakememchay = (req,res) => {
+const getburntcheesecakememchay = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('burntcheesecakememchay.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('burntcheesecakememchay.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('burntcheesecakememchay.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getchangasaot = (req,res) => {
+const getchangasaot = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('changasaot.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('changasaot.ejs', { loggedIn: false,username: null });
+  } else {
+    res.render('changasaot.ejs', { loggedIn: false, username: null });
+  }
 }
-}
-const getyenmachsuachua = (req,res) => {
+const getyenmachsuachua = (req, res) => {
   const loggedIn = req.session.user ? true : false;
   if (loggedIn) {
     const username = req.session.user.username
     res.render('yenmachsuachua.ejs', { loggedIn: true, username: username });
-} else {
-    res.render('yenmachsuachua.ejs', { loggedIn: false,username: null });
-}
+  } else {
+    res.render('yenmachsuachua.ejs', { loggedIn: false, username: null });
+  }
 }
 
-const getLogout = async (req,res) => {
+const getLogout = async (req, res) => {
   try {
     let isLogged = await isLogging(req);
     if (isLogged === false) {
-      return res.status(400).send({message: "You are not logged in."});
+      return res.status(400).send({ message: "You are not logged in." });
     }
     req.session.user = null;
     res.redirect('/');
   } catch (error) {
-    return res.status(500).send({error: "Server Error"});
+    return res.status(500).send({ error: "Server Error" });
   }
 }
 const getSearch = (request, response) => {
@@ -398,8 +371,8 @@ const getSearch = (request, response) => {
 }
 
 module.exports = {
-    getHomepage,getLogin,getMeoVat,getMeo,getMonansang,getMonantrua,
-    get4meobienthitdaithanhthitmem,get6luuychonguoimoibatdau,get6Skillslambep,get10bikipchonthucphamtuoi,getCachlamsangamdunnuocdien,getNauanvoingucoc,
-    getBuaSang,getBuaTrua,getbanhbao,getbanhtrungthu,getbunca,getburntcheesecakememchay,getchangasaot,getyenmachsuachua,getRegister,getProfile,
-     getLogout, getForgotPassword, getSearch
+  getHomepage, getLogin, getMeoVat, getMeo, getMonansang, getMonantrua,
+  get4meobienthitdaithanhthitmem, get6luuychonguoimoibatdau, get6Skillslambep, get10bikipchonthucphamtuoi, getCachlamsangamdunnuocdien, getNauanvoingucoc,
+  getBuaSang, getBuaTrua, getbanhbao, getbanhtrungthu, getbunca, getburntcheesecakememchay, getchangasaot, getyenmachsuachua, getRegister, getProfile,
+  getLogout, getForgotPassword, getSearch
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const {getHomepage} = require('../controllers/homeController');
-const {getLogin} = require('../controllers/homeController');
-const {getRegister} = require('../controllers/homeController');
-const {getProfile} = require('../controllers/homeController');
-const {getMeoVat} = require('../controllers/homeController');
-const {get4meobienthitdaithanhthitmem} = require('../controllers/homeController');
-const {get6luuychonguoimoibatdau} = require('../controllers/homeController');
-const {get6Skillslambep} = require('../controllers/homeController');
-const {get10bikipchonthucphamtuoi} = require('../controllers/homeController');
-const {getCachlamsangamdunnuocdien} = require('../controllers/homeController');
-const {getNauanvoingucoc} = require('../controllers/homeController');
-const {getBuaSang} = require('../controllers/homeController');
-const {getBuaTrua} = require('../controllers/homeController');
-const {getbanhbao} = require('../controllers/homeController');
-const {getbanhtrungthu} = require('../controllers/homeController');
-const {getbunca} = require('../controllers/homeController');
-const {getburntcheesecakememchay} = require('../controllers/homeController');
-const {getchangasaot} = require('../controllers/homeController');
-const {getyenmachsuachua} = require('../controllers/homeController');
-const {getLogout} = require('../controllers/homeController');
-const {getForgotPassword} = require('../controllers/homeController');
-const {getMeo} = require('../controllers/homeController');
-const {getMonansang} = require('../controllers/homeController');
-const {getMonantrua} = require('../controllers/homeController');
-const {getSearch} = require('../controllers/homeController');
+const { getHomepage } = require('../controllers/homeController');
+const { getLogin } = require('../controllers/homeController');
+const { getRegister } = require('../controllers/homeController');
+const { getProfile } = require('../controllers/homeController');
+const { getMeoVat } = require('../controllers/homeController');
+const { get4meobienthitdaithanhthitmem } = require('../controllers/homeController');
+const { get6luuychonguoimoibatdau } = require('../controllers/homeController');
+const { get6Skillslambep } = require('../controllers/homeController');
+const { get10bikipchonthucphamtuoi } = require('../controllers/homeController');
+const { getCachlamsangamdunnuocdien } = require('../controllers/homeController');
+const { getNauanvoingucoc } = require('../controllers/homeController');
+const { getBuaSang } = require('../controllers/homeController');
+const { getBuaTrua } = require('../controllers/homeController');
+const { getbanhbao } = require('../controllers/homeController');
+const { getbanhtrungthu } = require('../controllers/homeController');
+const { getbunca } = require('../controllers/homeController');
+const { getburntcheesecakememchay } = require('../controllers/homeController');
+const { getchangasaot } = require('../controllers/homeController');
+const { getyenmachsuachua } = require('../controllers/homeController');
+const { getLogout } = require('../controllers/homeController');
+const { getForgotPassword } = require('../controllers/homeController');
+const { getMeo } = require('../controllers/homeController');
+const { getMonansang } = require('../controllers/homeController');
+const { getMonantrua } = require('../controllers/homeController');
+const { getSearch } = require('../controllers/homeController');
 
 
 const route = require('color-convert/route');
@@ -31,53 +31,56 @@ const router = express.Router();
 
 // Middleware kiểm tra xem người dùng đã đăng nhập hay chưa
 const checkLoggedIn = (req, res, next) => {
-    // Kiểm tra xem người dùng đã đăng nhập hay chưa
-    if (req.session.loggedIn) {
-        // Nếu đã đăng nhập, gán thông tin người dùng vào req.locals để truy cập trong file view
-        req.locals.username = req.session.username;
-    }
-    // Tiếp tục xử lý các yêu cầu tiếp theo
-    next();
-  };
-router.get('/',checkLoggedIn,getHomepage);
+  // Kiểm tra xem người dùng đã đăng nhập hay chưa
+  if (req.session.loggedIn) {
+    // Nếu đã đăng nhập, gán thông tin người dùng vào req.locals để truy cập trong file view
+    req.locals.username = req.session.username;
+  }
+  // Tiếp tục xử lý các yêu cầu tiếp theo
+  next();
+};
+router.get('/', checkLoggedIn, getHomepage);
 
-router.get('/Login',getLogin);
-
-
-router.get('/Register',getRegister);
+router.get('/Login', getLogin);
 
 
-router.get('/Logout',getLogout);
+router.get('/Register', getRegister);
 
-router.get('/ForgotPassword',getForgotPassword);
-router.get('/Profile',getProfile);
+
+router.get('/Logout', getLogout);
+
+router.get('/ForgotPassword', getForgotPassword);
+router.get('/Profile', getProfile);
 
 // Trang chi tiet meo
-router.get('/MeoVat/4-meo-bien-thit-dai-thanh-thit-mem', get4meobienthitdaithanhthitmem); 
+router.get('/MeoVat/4-meo-bien-thit-dai-thanh-thit-mem', get4meobienthitdaithanhthitmem);
 router.get('/MeoVat/6-luu-y-cho-nguoi-moi-bat-dau', get6luuychonguoimoibatdau);
-router.get('/MeoVat/6-Skills-lam-bep', get6Skillslambep); 
-router.get('/MeoVat/10-bi-kip-chon-thuc-pham-tuoi', get10bikipchonthucphamtuoi); 
+router.get('/MeoVat/6-Skills-lam-bep', get6Skillslambep);
+router.get('/MeoVat/10-bi-kip-chon-thuc-pham-tuoi', get10bikipchonthucphamtuoi);
 router.get('/MeoVat/Cach-lam-sang-am-dun-nuoc-dien', getCachlamsangamdunnuocdien);
-router.get('/MeoVat/Nau-an-voi-ngu-coc', getNauanvoingucoc); 
+router.get('/MeoVat/Nau-an-voi-ngu-coc', getNauanvoingucoc);
 
 // Trang meo
-router.get('/MeoVat',getMeoVat);
-router.get('/MeoVat/:SLUG',getMeo);
+router.get('/MeoVat', getMeoVat);
+router.get('/MeoVat/:SLUG', getMeo);
+router.get('/MeoVat/navigate/:page', getMeoVat);
+
 // Trang mon an
-router.get('/BuaSang',getBuaSang);
-router.get('/BuaTrua',getBuaTrua);
+router.get('/BuaSang', getBuaSang);
+router.get('/BuaTrua', getBuaTrua);
 // Trang chi tiet mon an
-router.get('/BuaSang/Banh-bao', getbanhbao); 
-router.get('/BuaTrua/Banh-trung-thu', getbanhtrungthu); 
+router.get('/BuaSang/Banh-bao', getbanhbao);
+router.get('/BuaTrua/Banh-trung-thu', getbanhtrungthu);
 router.get('/BuaSang/Bun-ca', getbunca);
 router.get('/BuaTrua/burnt-cheese-cake-mem-chay', getburntcheesecakememchay);
 router.get('/BuaTrua/chan-ga-sa-ot', getchangasaot);
 router.get('/BuaSang/yen-mach-sua-chua', getyenmachsuachua);
 
-router.get('/BuaSang/:SLUG',getMonansang);
-router.get('/BuaTrua/:SLUG',getMonantrua);
-
-router.get('/search', getSearch); 
+router.get('/BuaSang/:SLUG', getMonansang);
+router.get('/BuaTrua/:SLUG', getMonantrua);
+router.get('/BuaSang/navigate/:page', getBuaSang);
+router.get('/BuaTrua/navigate/:page', getBuaTrua);
+router.get('/search', getSearch);
 
 
 module.exports = router; 
